1cb739408bf78b0c387afa3a71dad225ab0646fe
seeker-zuo <seeker-zuo@users.noreply.github.com>
2026-09-18 07:35:41 +0000

    deploy: 516a74f33bc29fbb288ff0cef00b4d849c7db8d6

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9b6f73ba.js',
+                path_to_root + 'searchindex-741bfb75.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
