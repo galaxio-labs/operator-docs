1fb80b689de916400709ce17df45def75d4ce8ae
seeker-zuo <seeker-zuo@users.noreply.github.com>
2026-09-18 04:39:27 +0000

    deploy: 751611486211464c1613f1abe128f1d2910b1dcf

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-abd8e62c.js',
+                path_to_root + 'searchindex-8a530cac.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
